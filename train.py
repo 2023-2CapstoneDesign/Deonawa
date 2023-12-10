5a9e4cdae43e57e487e7059b869634c5f0c31964
chrisheo <jftj97@gmail.com>
2023-12-10 20:09:24 +0900

    [feat] roboflow model added

--- train.py
+++ train.py
@@ -5,4 +5,4 @@ import os
 os.system('python yolov5/train.py --img 640 --batch 16 --epochs 30 --data data.yaml --weights yolov5s.pt')
 
 # test
-os.system('python yolov5/detect.py --weights yolov5s.pt --img 640 --conf 0.25 --source test/images')
+#os.system('python yolov5/detect.py --weights yolov5s.pt --img 640 --conf 0.25 --source test/images')
--- yolov5.py
+++ yolov5.py
@@ -6,48 +6,86 @@ import numpy as np
 import torch
 from pathlib import Path
 
-# https://public.roboflow.com/object-detection/website-screenshots
-# Function to crop and save detected elements
-def crop_and_save(product_name,img, box, label, save_dir='cropped_elements'):
+from roboflow import Roboflow
+
+# Initialize Roboflow
+rf = Roboflow(api_key="r3nfTGPGBAVKxQlVgkbV")
+project = rf.workspace().project("deonawa")
+model = project.version(3).model
+
+def crop_and_save(product_name, img, box, label, save_dir='cropped_images', x_offset=0, y_offset=0):
     x1, y1, x2, y2 = map(int, box)
+
+    # Adjust the coordinates by subtracting the offset values
+    x1 -= x_offset
+    x2 -= x_offset
+    y1 -= y_offset
+    y2 -= y_offset
+
     cropped_img = img[y1:y2, x1:x2]
+    print(f"Image shape: {cropped_img.shape}")
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    save_path = os.path.join(save_dir, f"{product_name}_{label}.png")
+    # Extract only the filename without the path
+    filename = f"{product_name}_{label}.png"
+    save_path = os.path.join(save_dir, filename)
+    print(f"Saving to: {save_path}")
     cv2.imwrite(save_path, cropped_img)
-    return save_path  # Return the path of the saved image
+    print(f"Save complete for {product_name}_{label}")
+    return save_path
+
+
+
+
 
 # Function to apply Tesseract OCR on a single image
 def apply_ocr(image_path):
     img = cv2.imread(image_path)
+    
+    # Add print statements to check image loading
+    if img is None:
+        print(f"Error: Unable to load image at {image_path}")
+        return ""
+
     rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     text = pytesseract.image_to_string(rgb_image, lang='kor')
     return text
 
-# Function to process a directory of images
-def process_directory(directory):
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='/Users/yeonu/PycharmProjects/Deonawa/yolov5s.pt', force_reload=True)
 
-    for img_file in Path(directory).rglob('*.png'):  # Assuming all images are PNG
+def process_directory(directory):
+    for img_file in Path(directory).rglob('*.png'):
         print(f"Processing image: {img_file}")
         img = Image.open(img_file)
         img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
-        results = model(img)
+        # Get predictions
+        predictions = model.predict(str(img_file), confidence=40, overlap=30).json()
 
-        boxes = results.xyxy[0].cpu().numpy()  # Bounding boxes
-        for i, box in enumerate(boxes):
-            x1, y1, x2, y2, conf, cls = box
-            label = f"{model.names[int(cls)]}_{i}"
-            # Extract only the product name without the path
-            product_name = str(img_file).split('/')[-1]
-            product_name = product_name.replace(".png", "")
-            cropped_image_path = crop_and_save(product_name, img_cv, (x1, y1, x2, y2), label)
+        for prediction in predictions['predictions']:
+            x, y, width, height = prediction['x'], prediction['y'], prediction['width'], prediction['height']
+            confidence = prediction['confidence']
+            class_name = prediction['class']
+
+            # Check if the class index is within the valid range
+            label = f"{prediction['class']}"
+
+
+            # Extract only the product name without any path information
+            product_name = Path(img_file).stem  # This extracts the filename without extension
+            print(product_name)
+            # Adapt crop_and_save for Roboflow predictions
+            # Adjust the offset values based on your requirements
+            x_offset = 80  # Adjust as needed
+            y_offset = 20  # Adjust as needed
+
+            # Use the adjusted crop_and_save function
+            cropped_image_path = crop_and_save(product_name, img_cv, (x, y, x + width, y + height), label, "cropped_images", x_offset, y_offset)
+            #cropped_image_path = crop_and_save(product_name, img_cv, (x, y, x + width, y + height), label)
 
             ocr_text = apply_ocr(cropped_image_path)
-            print(f"OCR result for {label}: {ocr_text}")
-
+            print(f"OCR result for {label}: {ocr_text}") 
 
 # Replace this with your actual images directory path
-images_directory = '/Users/yeonu/PycharmProjects/Deonawa/images'
+images_directory = 'images'
+
 process_directory(images_directory)
